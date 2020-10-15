1ed8d1926b2b243fc46030c8ca5d48a6787ae913
Anderson Enrique Nevao <anderson.nevao810.4@hotmail.com>
2020-10-14 23:16:09 -0500

    adelantos interfaz

--- src/main/java/edu/cundi/ejerciciomvcwithmaven/vista/Interfaz.java
+++ src/main/java/edu/cundi/ejerciciomvcwithmaven/vista/Interfaz.java
@@ -5,10 +5,19 @@
  */
 package edu.cundi.ejerciciomvcwithmaven.vista;
 
+import edu.cundi.ejerciciomvcwithmaven.modelo.Carro;
+import java.util.ArrayList;
+import javax.swing.*;
+
+
 /**
  *
  * @author LENOVO
  */
 public class Interfaz {
-    
+     ArrayList<Carro> vehiculo = new  ArrayList<Carro>();
+    public JTextField placa,modelo,tipo;
+    public JButton insertar ,modificar , eliminar, mostrar,salir;
+    JLabel titulo ,titulo1,titulo2,titulo3,titulo4;
+    Carro car = new Carro();
 }
