125d189ce868b2ad08b705ae62a3930e1cc973d6
Anderson Enrique Nevao <anderson.nevao810.4@hotmail.com>
2020-10-14 23:06:25 -0500

    clase carro con atributos y sus respectivos getters y setters

--- src/main/java/edu/cundi/ejerciciomvcwithmaven/modelo/Carro.java
+++ src/main/java/edu/cundi/ejerciciomvcwithmaven/modelo/Carro.java
@@ -10,5 +10,65 @@ package edu.cundi.ejerciciomvcwithmaven.modelo;
  * @author LENOVO
  */
 public class Carro {
+    private int modelo;
+    private String tipo;
+    private String placa;
+
+    public Carro(int modelo, String tipo, String placa) {
+        this.modelo = modelo;
+        this.tipo = tipo;
+        this.placa = placa;
+    }
+
+    public Carro() {
+    }
+
     
+
+    /**
+     * @return the modelo
+     */
+    public int getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nmodelo: " + modelo + "\n tipo: "+ tipo + "\nplaca: " + placa;
+    }
 }
